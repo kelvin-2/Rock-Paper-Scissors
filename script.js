708042ba089d98e180e730646643a1a64f4eac50
Kelvin <kelvinyounglee47@gmail.com>
2024-10-23 13:33:35 +0200

    linked script to html

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ function getComputerChoice()
     var thing = things[Math.floor(Math.random()*things.length)];
     return thing;
 }
+function getHumanChoice(choice){
+    let pcChoicie=getComputerChoice();
+    if(pcChoice === choice) {
+        console.log("We have a tie");
+        alert("We have a tie");  // Use alert to display the message to the user
+    } 
+    else if(choice === "rock" && pcChoice === "Paper") {
+        console.log(pcChoice + " PC wins ");
+        alert(pcChoice + " PC wins ");
+    } 
+    else if(choice === "rock" && pcChoice === "Scissors") {
+        console.log("You win!");
+        alert("You win!");
+    } 
+    else if(choice === "paper" && pcChoice === "Rock") {
+        console.log("You win!");
+        alert("You win!");
+    } 
+    else if(choice === "paper" && pcChoice === "Scissors") {
+        console.log(pcChoice + " PC wins ");
+        alert(pcChoice + " PC wins ");
+    } 
+    else if(choice === "scissors" && pcChoice === "Rock") {
+        console.log(pcChoice + " PC wins ");
+        alert(pcChoice + " PC wins ");
+    } 
+    else if(choice === "scissors" && pcChoice === "Paper") {
+        console.log("You win!");
+        alert("You win!");
+    }
+    
+}
